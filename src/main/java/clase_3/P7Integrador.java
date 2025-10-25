be747fada9a4ccdbae1e0005a76a893f34fd838b
roxy979 <contrerasrosana7@gmail.com>
2025-10-25 13:09:04 -0300

    Se agrega ejercicio 7

--- src/main/java/clase_3/P7Integrador.java
+++ src/main/java/clase_3/P7Integrador.java
@@ -1,5 +1,7 @@
 package clase_3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 /*
 Ingresar por consola un número mientras sea menor o igual a 500 y
@@ -12,6 +14,25 @@ diarios”
 public class P7Integrador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numero;
+        List<Integer>ListaDeNumeros= new ArrayList<>();
+        int contador = 0;
+        do {
+            System.out.println("Ingrese el numero: ");
+            numero = scanner.nextInt();
+            ListaDeNumeros.add(numero);
+            contador++;
+        }while (numero <=500);
+        System.out.println("Datos ingresados: " + ListaDeNumeros);
 
+        if(ListaDeNumeros.size() <10){
+            System.out.println("S" +
+                    "Se ingresaron pocos números");
+
+        }else
+            System.out.println("Se excedió la cantidad de ingresos" +
+                    "" +
+                    "diarios");
     }
 }
+
